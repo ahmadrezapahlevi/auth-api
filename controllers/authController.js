4a7cf1af97c9d0ddb1df0f3bd9dfeed0407074c6
ahmadrezapahlevi <rezalevi95752@gmail.com>
2025-04-16 17:01:32 +0700

    sign in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
-const { signupSchema } = require("../middlewares/validator");
+const jwt = require('jsonwebtoken');
+const { signupSchema, signinSchema} = require("../middlewares/validator");
 const usersModel = require("../models/usersModel");
-const { doHash } = require("../utils/hashing");
+const { doHash, doHashValidation } = require("../utils/hashing");
 
 exports.signup = async (req, res) => {
     const { email, password } = req.body;
@@ -37,16 +38,33 @@ exports.signup = async (req, res) => {
 exports.signin = async (req, res) => {
     const {email, password} = req.body;
     try{
-        const {error, value} = signinSchema({email, password});
+        const { error, value } = signinSchema.validate({ email, password });
 
         if(error){
             return res.status(401).json({ success: false, message: error.details[0].message })
         }
 
-        const existingUser = await UserActivation.findOne({email}).select('+password')
+        const existingUser = await usersModel.findOne({ email }).select('+password');
         if(!existingUser){
             return res.status(401).json({ success: false, message: 'Pengguna tidak ada' })
         }
+        const result = await doHashValidation(password, existingUser.password)
+        if(!result){
+          return res.status(401).json({ success: false, message: 'invalid credentials' })
+        }
+        const token = jwt.sign({
+          userId: existingUser._id,
+          email: existingUser.email,
+          verified: existingUser.verified,
+        }, 
+        process.env.TOKEN_SECRET
+        );
+        
+        res.cookie('Authorization', 'Bearer ' + token, { expires: new Date(Date.now() + 8 * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV === 'production'}).json({
+          success:true,
+          token,
+          message:'login berhasil',
+        })
         
     } catch (error) {
         console.log(error);
