ab98c54080c215f0ae522348ce590e7479b7f318
ahmadrezapahlevi <rezalevi95752@gmail.com>
2025-04-16 23:58:54 +0700

    send verification code

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
-const { signupSchema, signinSchema} = require("../middlewares/validator");
-const usersModel = require("../models/usersModel");
-const { doHash, doHashValidation } = require("../utils/hashing");
+const { signupSchema, signinSchema } = require("../middlewares/validator");
+const User = require("../models/usersModel");
+const { doHash, doHashValidation, hmacProcess } = require("../utils/hashing");
+const transport = require('../middlewares/sendMail');
 
 exports.signup = async (req, res) => {
     const { email, password } = req.body;
@@ -11,7 +12,7 @@ exports.signup = async (req, res) => {
         if (error) {
             return res.status(401).json({ success: false, message: error.details[0].message })
         }
-        const existingUser = await usersModel.findOne({ email });
+        const existingUser = await User.findOne({ email });
 
         if (existingUser) {
             return res.status(401).json({ success: false, message: 'Pengguna sudah ada' })
@@ -19,7 +20,7 @@ exports.signup = async (req, res) => {
 
         const hashedPassword = await doHash(password, 12);
 
-        const newUser = new usersModel({
+        const newUser = new User({
             email,
             password: hashedPassword,
         })
@@ -44,7 +45,7 @@ exports.signin = async (req, res) => {
             return res.status(401).json({ success: false, message: error.details[0].message })
         }
 
-        const existingUser = await usersModel.findOne({ email }).select('+password');
+        const existingUser = await User.findOne({ email }).select('+password');
         if(!existingUser){
             return res.status(401).json({ success: false, message: 'Pengguna tidak ada' })
         }
@@ -57,7 +58,10 @@ exports.signin = async (req, res) => {
           email: existingUser.email,
           verified: existingUser.verified,
         }, 
-        process.env.TOKEN_SECRET
+        process.env.TOKEN_SECRET,
+        {
+          expiresIn: '8h',
+        }
         );
         
         res.cookie('Authorization', 'Bearer ' + token, { expires: new Date(Date.now() + 8 * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV === 'production'}).json({
@@ -70,4 +74,51 @@ exports.signin = async (req, res) => {
         console.log(error);
 
     }
+}
+
+exports.signout = async (req, res) => {
+  res
+  .clearCookie('Authorization')
+  .status(200)
+  .json({
+    success: true,
+    message: 'berhasil keluar dari akun'
+  })
+}
+
+exports.sendVerificationCode = async (req, res) => {
+  const {email} = req.body;
+  try {
+    const existingUser = await User.findOne({email})
+            if(!existingUser){
+            return res.status(404).json({ success: false, message: 'Pengguna tidak ada' })
+        }
+        if(existingUser.verified){
+          return res.status(400).json({ success: false, message: 'Kamu sudah ter verifikasi' })
+        }
+        const codeValue = Math.floor(Math.random() * 1000000).toString();
+        let info = await transport.sendMail({
+          from: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
+          to: existingUser.email,
+          subject: 'verification code',
+          html:'<h1>' + codeValue + '</h1>',
+        })
+        
+        if(info.accepted[0] === existingUser.email) {
+          const hashedCodeValue = hmacProcess(codeValue, process.env.MAC_VERIFICATION_CODE_SECRET)
+          existingUser.verificationCode = hashedCodeValue;
+          existingUser.verificationCodeValidation = Date.now();
+          await existingUser.save()
+          return res.status(200).json({
+            success: true,
+            message: 'code sent',
+          })
+        }
+        return res.status(400).json({
+            success: false,
+            message: 'code sent failed',
+          })
+  } catch (error) {
+    console.log(error);
+  }
 }
